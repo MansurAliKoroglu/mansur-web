b3fca3211e75933dd207bb1b5b2bde211cae0bc4
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-11-20 00:11:52 +0300

    Remove bootstrap vue

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+  }
+
   #app {
     font-family: 'Poppins', sans-serif;
   }
