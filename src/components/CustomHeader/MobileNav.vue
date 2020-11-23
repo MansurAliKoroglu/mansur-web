4dd3e8b053be0d736350ed0e1e553733e6929c83
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-11-23 22:56:59 +0300

    Split mobile and desktop navs to components

---
+++ src/components/CustomHeader/MobileNav.vue
@@ -0,0 +1,72 @@
+<template>
+  <fragment>
+    <font-awesome-icon
+      :icon="['fas', 'bars']"
+      @click="toggleDrawer"
+    />
+    <transition name="drawer-toggle">
+      <nav v-show="isDrawerOpened">
+        <div class="drawer-header">
+          <font-awesome-icon :icon="['fas', 'bars']" @click="toggleDrawer" />
+        </div>
+        <hr />
+      </nav>
+    </transition>
+  </fragment>
+</template>
+
+<script>
+  import { Fragment } from 'vue-fragment';
+
+  export default {
+    components: {
+      Fragment
+    },
+    data() {
+      return {
+        isDrawerOpened: false
+      };
+    },
+    methods: {
+      toggleDrawer() {
+        this.isDrawerOpened = !this.isDrawerOpened;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  @import '../assets/styles/variables.css';
+
+  nav {
+    width: 75%;
+    max-width: 400px;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transform: translateX(0);
+    background-color: var(--header-color);
+    display: flex;
+    flex-direction: column;
+  }
+
+  .drawer-header {
+    height: var(--header-height);
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+  }
+
+  nav hr {
+    width: 85%;
+  }
+
+  .drawer-toggle-enter-active, .drawer-toggle-leave-active {
+    transition: transform .5s;
+  }
+
+  .drawer-toggle-enter, .drawer-toggle-leave-to {
+    transform: translateX(100%);
+  }
+</style>
---
+++ src/components/CustomHeader/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <div class="logo">MA</div>
+    <mq-layout :mq="['xs', 'sm', 'md']">
+      <mobile-nav />
+    </mq-layout>
+    <mq-layout mq="lg+">
+      <desktop-nav />
+    </mq-layout>
+  </header>
+</template>
+
+<script>
+  import MobileNav from './MobileNav';
+  import DesktopNav from './DesktopNav';
+
+  export default {
+    components: {
+      MobileNav,
+      DesktopNav
+    }
+  };
+</script>
+
+<style scoped>
+  @import '../assets/styles/variables.css';
+
+  header {
+    width: 100%;
+    height: var(--header-height);
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--header-color);;
+    color: white;
+    padding: 0 1rem;
+    box-shadow: 0px 0px 9px 3px rgba(41, 41, 41, .5);
+  }
+
+  .logo {
+    user-select: none;
+    font-size: 1.7rem;
+    font-weight: bold;
+  }
+</style>
