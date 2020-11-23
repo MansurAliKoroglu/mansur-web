376dd9ee3c52b49ad86341fbdb1e4caf4bedc728
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-11-23 21:54:03 +0300

    Install and configure vue mq

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue';
+import VueMq from 'vue-mq';
 
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  VueMq,
+  {
+    breakpoints: {
+      xs: 576,
+      sm: 768,
+      md: 992,
+      lg: 1200,
+      xl: Infinity
+    },
+    defaultBreakpoint: 'xs'
+  }
+);
 
 new Vue({
   router,
